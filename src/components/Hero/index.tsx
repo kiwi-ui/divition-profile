5253a3fb9539ee38d40434cd80d61bdbceac3b03
yusni anggara <yusnianggara@gmail.com>
2024-09-04 20:50:24 +0700

    fix

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,12 @@
-// import hero from '../../assets/images/hero.png'
+import hero from '../../assets/images/hero.png'
 export const Hero = () => {
   return (
     <div className="position-absolute vh-100 d-flex justify-content-center align-items-center top-0 w-100">
       <div className="container px-4">
         <div className="d-flex flex-lg-row flex-column justify-content-around align-items-center gap-5 py-0">
-          {/* <img className={`w-50`} src={hero}/> */}
+          <img className={`w-50`} src={hero}/>
           
-          <div className="text-center text-lg-start mt-3 mt-md-0 text-white">
+          <div className="mt-3 mt-md-0 text-white">
               <p className="fw-bolder mb-4 fs-1">Crafted for Unforgettable Moments.</p>
               <p className='m-0'>Discover our elegant wedding invitation templates to add a touch of charm to your special day. Choose from a variety of styles and themes, easily customize, and let your love story shine through every detail.</p>
           </div>
